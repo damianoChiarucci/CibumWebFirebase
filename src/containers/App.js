41c9bc207f2b9044ee025547c806208249dbce13
Damiano Chiarucci <damianochi@gmail.com>
2020-12-11 11:47:04 +0100

    lezione 10/12/2020

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// importiamo gli elementi di material ui che ci occorrono : il menu vero e proprio e gli elementi list, list item e list text per stilizzare i bottoni che avremo nel menu
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-
+import MenuIcon from '@material-ui/icons/Menu';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Menu from '../components/Menu';
 import styled from 'styled-components';
 
 function App() {
   // stato che utilizzeremo per aprire e chiudere il nostro menu laterale. Il menu può solo essere aperto o chiudo, perciò utilizzo un booleano (true/aperto, false/chiuso)
   const [menuVisibile, setMenuVisibile] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
   const apriChiudiMenu = () => {
     // con il punto esclamativo prima di una variabile andiamo a selezionare il valore opposto di un booleano 
@@ -19,54 +21,47 @@ function App() {
     setMenuVisibile(!menuVisibile);
   };
 
+  if (loading) {
+    return (
+      <ContenitoreLoading>
+        <CircularProgress />
+      </ContenitoreLoading>
+    );
+  }
   return (
-    <Wrapper className="App">
+    
+    <Contenitore className="App">
       <header className="app-header">
         {/* questo bottone determina l'apertura o la chiusura del menu*/}
-        <div onClick={() => apriChiudiMenu()}>
-          CLICCAMI
-        </div>
+        <MenuIcon onClick={() => apriChiudiMenu()} />
+        <Menu menuVisibile={menuVisibile} apriChiudiMenu={apriChiudiMenu} />
       </header>
-      {/* 
-        qui inizia il menu, con l'elemento SwipeableDrawer che contiene tutti i bottoni con i nomi delle sezione della mia APP 
-        anchor indica la posizione da cui il menu si aprirà
-        open indica se il menu è aperto o chiuso
-        onClose e onOpen sono eventi legati allo swipe dell'utente (in generale per noi è importante invocare una sola funzione: apriChiudiMenu)
-      */}
-      <SwipeableDrawer
-        anchor="right"
-        open={menuVisibile}
-        onClose={() => apriChiudiMenu()}
-        onOpen={() => apriChiudiMenu()}
-      >
-        {/* List svolge il ruolo di <ul> e ListItem quello di <li>: in questo caso potrei utilizzare una costante ed eseguire un .map per ciclarmi tutte le pagine della mia app che voglio elencare */}
-        <List>
-          <ListItem button key="Home">
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem button key="Ricette">
-            <ListItemText primary="Ricette" />
-          </ListItem>
-          <ListItem button key="Lista della Spesa">
-            <ListItemText primary="Lista della Spesa" />
-          </ListItem>
-        </List>
-      </SwipeableDrawer>
-    </Wrapper>
+    </Contenitore>
   );
 }
 
-const Wrapper = styled.div`
+const Contenitore = styled.div`
+
   .app-header {
     background-color: #e0902c;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-end;
     padding: 20px;
     font-size: 20px;
     color: white;
   }
 
+`;
+
+const ContenitoreLoading = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    .MuiCircularProgress-colorPrimary {
+      color: #e0902c !important;
+    }
 `;
 
 export default App;
